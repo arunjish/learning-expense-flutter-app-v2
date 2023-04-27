675980b17b8cbd75710c0e9ce20f7dc1c8b758a2
Arun jishnu <lettertojishnu@gmail.com>
2023-04-27 21:10:51 +0530

    102 list view

---
+++ lib/widgets/expense_list/expense_list.dart
@@ -0,0 +1,22 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expense_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseList extends StatelessWidget {
+  const ExpenseList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        return ExpenseItem(expenses[index]);
+      },
+    );
+  }
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,123 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+import 'expense_list/expense_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "New shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Weekly groceries",
+      amount: 16.53,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "New phone",
+      amount: 599.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New desk",
+      amount: 299.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New monitor",
+      amount: 399.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Trip to the beach",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "New headphones",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "New chair",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New mouse",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New keyboard",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New microphone",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New processor",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New hard drive",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New graphics card",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New motherboard",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "New SSD",
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        const Text("Chart"),
+        Expanded(child: ExpenseList(expenses: _registeredExpenses))
+      ]),
+    );
+  }
+}
